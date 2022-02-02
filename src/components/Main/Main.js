ad4cc89ce2a145a40e9c753d2f9b11ce81a938c6
Tiago BR <ti.volei@hotmail.com>
2022-02-02 19:14:06 -0300

    ajustes na responsividade dos elementos

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -18,7 +18,7 @@ const Main = () => {
           src={Video}
           autoPlay={true}
           loop 
-                 
+          muted
           className={styles.video}
         ></video>
       </div>
