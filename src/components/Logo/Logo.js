e16e338a214878fccde58f339c0e4d8821ea2a96
Tiago BR <ti.volei@hotmail.com>
2022-02-19 15:27:54 -0300

    sound effect add aos menus

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,14 @@
 import styles from "./Logo.module.scss";
 import IMG from "../../images/logo.png";
 
+import useSound from 'use-sound';
+import soundMenu from '../../audios/Resident Evil 2 Voice Title.m4a'
+
 const Logo = () => {
+  const [effect] = useSound(soundMenu)
   return (
     <div className={styles.logo}>
-      <img src={IMG} alt={Logo} />
+      <img src={IMG} alt={Logo} onClick={effect} />
     </div>
   );
 };
