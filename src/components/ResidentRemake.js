3893aa87e6d1696a996c0cdcbd6ff3d3de5059b0
Tiago Silva <ti.volei@hotmail.com>
2022-10-06 11:21:53 -0300

    Efeito hover add ao Aside e responsividade ajustada

--- src/components/ResidentRemake.js
+++ src/components/ResidentRemake.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 import "../styles/app.scss";
 
-import Header from '../pages/ResidentRemake/Header/Header'
+import Header from '../pages/ResidentRemake/Header/Header';
 import Hero from '../pages/ResidentRemake/Hero/Hero';
-import Story from '../pages/ResidentRemake/Story/StoryWrapper'
+import Story from '../pages/ResidentRemake/Story/StoryWrapper';
+import FixedBg from '../pages/ResidentRemake/FixedBg/FixedBg';
 
 function ResidentRemake() {
   return (
@@ -12,6 +13,7 @@ function ResidentRemake() {
       <Header />
       <Hero />
       <Story />
+      <FixedBg />
     </>
   )
 }
