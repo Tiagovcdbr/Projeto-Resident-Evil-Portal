3aa4af77cff05e5a792f670517c230441727443f
Tiago Silva <ti.volei@hotmail.com>
2022-10-09 18:39:59 -0300

    component History add ao Portal versão responsiva um pouco desfocada mas visivel

--- src/pages/Home/Articles/ContentsKnowAbout/About.js
+++ src/pages/Home/Articles/ContentsKnowAbout/About.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import styles from './About.module.scss';
+import Fade from 'react-reveal/Fade';
 
 function About() {
   return (
-    <div>About</div>
+    <div className={styles.know_about}>
+      <div className={styles.inner}>
+        <Fade bottom duration={2500}>
+          <div className={styles.about_left}>
+            <h3>HISTORY</h3>
+            <p>
+              Capcom released Resident Evil in 1996
+              <br>
+              </br>
+              and forever defined the world of survival horror.
+            </p>
+            <a className={styles.btn} href="about-portal.html">More</a>
+          </div>
+        </Fade>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home/Articles/Contentsknow.js
+++ src/pages/Home/Articles/Contentsknow.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from './ContentsKnow.module.scss';
+import About from './ContentsKnowAbout/About';
 import News from './ContentsKnowNews/News';
 
 function Contentsknow() {
   return (
     <div className={styles.contents_know}>
       <News />
+      <About />
     </div>
   )
 }
