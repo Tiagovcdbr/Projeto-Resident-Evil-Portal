8e89841c38dcfc0d2b4560564facc28fc0e4d8e1
Tiago Silva <ti.volei@hotmail.com>
2022-10-09 20:25:38 -0300

    Efeito Fade add ao About history no Portal

--- src/pages/Home/Articles/ContentsKnowAbout/About.js
+++ src/pages/Home/Articles/ContentsKnowAbout/About.js
@@ -5,19 +5,24 @@ import Fade from 'react-reveal/Fade';
 function About() {
   return (
     <div className={styles.know_about}>
-      <div className={styles.inner}>
-        <Fade bottom duration={2500}>
+      <div className={styles.inner}>        
           <div className={styles.about_left}>
-            <h3>HISTORY</h3>
-            <p>
-              Capcom released Resident Evil in 1996
+            <Fade bottom duration={2200}>
+              <h3>HISTORY</h3>
+            </Fade>
+            <Fade bottom duration={2400}>
+            <p>            
+              Capcom released Resident Evil in 1996            
               <br>
-              </br>
-              and forever defined the world of survival horror.
+              </br>           
+              and forever defined the world of survival horror.           
             </p>
-            <a className={styles.btn} href="about-portal.html">More</a>
+            </Fade>
+            <Fade bottom duration={2900}>
+              <a className={styles.btn} href="about-portal.html">More</a>
+            </Fade>
           </div>
-        </Fade>
+        
       </div>
     </div>
   )
