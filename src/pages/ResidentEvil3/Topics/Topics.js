a8a9de15335b9d9e8ac2a01b6e8f831030d140ad
Tiago Silva <ti.volei@hotmail.com>
2023-01-03 13:52:53 -0300

    Update na seção Topics

--- src/pages/ResidentEvil3/Topics/Topics.js
+++ src/pages/ResidentEvil3/Topics/Topics.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import SlickList from '../../../utils/SlickList/SlickList'
 import styles from '../Topics/Topics.module.scss'
@@ -7,7 +8,14 @@ function Topics() {
     <div className={styles.conts_box_topics}>
       <h2 className={styles.sec_ttl_topics}>NEWS</h2>
         <div className={styles.conts_box_inner_topics}>
-          <SlickList />
+          <SlickList/>
+          <div className={styles.link_btn_topics}>
+            <div className={styles.btn_topics}>
+              <a>
+                <span>Update information</span>
+              </a>
+            </div>
+          </div>
         </div>
     </div>
   )
